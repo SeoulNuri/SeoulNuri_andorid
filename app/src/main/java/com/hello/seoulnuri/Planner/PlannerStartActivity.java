95ca2aceedd8ec8b6a22584ca6f81b2ac1e7d508
장용범 <dnwkqk@gmail.com>
2018-08-18 21:37:42 +0900

    update plannerstartactivity

---
+++ app/src/main/java/com/hello/seoulnuri/Planner/PlannerStartActivity.java
@@ -0,0 +1,15 @@
+package com.hello.seoulnuri.Planner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hello.seoulnuri.R;
+
+public class PlannerStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner_start);
+    }
+}

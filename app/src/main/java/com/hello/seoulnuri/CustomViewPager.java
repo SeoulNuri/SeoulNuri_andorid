dcb08f44e161d2762589d5562dddb163004cd4af
carrot <yajya004@gmail.com>
2018-08-20 22:55:20 +0900

    1. Fill fragment_mypage.xml 2. Disable ViewPage Swipe

---
+++ app/src/main/java/com/hello/seoulnuri/CustomViewPager.java
@@ -0,0 +1,39 @@
+package com.hello.seoulnuri;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class CustomViewPager extends ViewPager {
+    private boolean enabled;
+
+    public CustomViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (this.enabled) {
+            return super.onTouchEvent(ev);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(ev);
+        }
+
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}

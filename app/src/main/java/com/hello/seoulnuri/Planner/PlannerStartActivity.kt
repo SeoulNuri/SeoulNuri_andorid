bbe31f426b09831558b4ce0041360d1fb64575f0
장용범 <dnwkqk@gmail.com>
2018-08-28 20:43:53 +0900

    update planner autocompletetextview

--- app/src/main/java/com/hello/seoulnuri/Planner/PlannerStartActivity.kt
+++ app/src/main/java/com/hello/seoulnuri/Planner/PlannerStartActivity.kt
@@ -1,11 +1,15 @@
 package com.hello.seoulnuri.Planner
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.hello.seoulnuri.R
 import com.hello.seoulnuri.utils.ToastMaker
 import kotlinx.android.synthetic.main.activity_planner_start.*
@@ -50,6 +54,31 @@ class PlannerStartActivity : AppCompatActivity(), View.OnClickListener{
         planner_start_next_button.setOnClickListener(this)
     }
 
+    fun autoInit(){
+        val colors = arrayOf(
+                "Red","Green","Blue","Maroon","Magenta",
+                "Gold","GreenYellow"
+        )
+
+        val adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                colors
+        )
+
+        search_auto_text.setAdapter(adapter)
+
+        search_auto_text.threshold = 1
+
+        search_auto_text.onItemClickListener = AdapterView.OnItemClickListener{
+            parent,view,position,id->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+
+            search_auto_text.text = Editable.Factory.getInstance().newEditable(selectedItem)
+
+        }
+    }
+
     internal var mNow: Long = 0
     internal var mDate = Date(mNow)
     internal var mFormat = SimpleDateFormat("MM월 dd일")
@@ -58,11 +87,12 @@ class PlannerStartActivity : AppCompatActivity(), View.OnClickListener{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planner_start)
         init()
+        autoInit()
 
 
         search_auto_text.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-
+                search_auto_text.background = ColorDrawable(Color.TRANSPARENT)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {

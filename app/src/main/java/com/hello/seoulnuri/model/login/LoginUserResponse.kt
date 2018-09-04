cf370b400789d34f598aab59ff456f1c48d9eca6
Woo <jhsw0375@gmail.com>
2018-09-05 00:42:02 +0900

    Login 통신 성공 및 토큰 받기 성공 그리고 BaseModel 추가

---
+++ app/src/main/java/com/hello/seoulnuri/model/login/LoginUserResponse.kt
@@ -0,0 +1,10 @@
+package com.hello.seoulnuri.model.login
+
+import com.hello.seoulnuri.base.BaseModel
+
+/**
+ * Created by VictoryWoo
+ */
+data class LoginUserResponse (
+        var data : String?
+) : BaseModel()

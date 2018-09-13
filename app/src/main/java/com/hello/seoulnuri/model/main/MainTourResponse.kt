8c1a67438829b6cf15eec6da0beb52d918a5c450
Woo <jhsw0375@gmail.com>
2018-09-13 19:34:18 +0900

    로그인 카테고리, 메인 정보 불러오기 통신 연결 메인에서 정보가 없는 듯 싶다.

---
+++ app/src/main/java/com/hello/seoulnuri/model/main/MainTourResponse.kt
@@ -0,0 +1,14 @@
+package com.hello.seoulnuri.model.main
+
+import com.hello.seoulnuri.base.BaseModel
+
+/**
+ * Created by VictoryWoo
+ */
+data class MainTourResponse(
+        var data : MainRecoTourData
+) : BaseModel()
+
+
+
+
--- app/src/main/java/com/hello/seoulnuri/network/NetworkService.kt
+++ app/src/main/java/com/hello/seoulnuri/network/NetworkService.kt
@@ -4,8 +4,10 @@ import com.hello.seoulnuri.base.BaseModel
 import com.hello.seoulnuri.model.login.LoginCategoryRequest
 import com.hello.seoulnuri.model.login.LoginUserData
 import com.hello.seoulnuri.model.login.LoginUserResponse
+import com.hello.seoulnuri.model.main.MainTourResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -15,17 +17,29 @@ import retrofit2.http.POST
 interface NetworkService {
     // 통신에 사용할 함수를 정의
 
-    // 1. 회원가입
+    // 1. 회원가입 - 0
     @POST("api/user/signup")
-    fun signUp(
-            @Body loginUserData: LoginUserData
-    ) : Call<LoginUserResponse>
+    fun signUp(@Body loginUserData: LoginUserData) : Call<LoginUserResponse>
 
 
-    // 2. 로그인 카테고리 선택
+
+    // 2. 로그인 카테고리 선택 - 0
     @POST("api/user")
     fun selectCategory(
             @Header("token") token : String,
             @Body loginCategoryRequest: LoginCategoryRequest
     ) : Call<BaseModel>
+
+
+    // 3. 메인에서 정보 받아오기 - 0 , 근데, 서버에서 값을 뿌려 줄 때 조금 이상한 것 같음. 값이 없음
+    @GET("api/main")
+    fun getMainInfo(
+            @Header("token") token : String
+    ) : Call<MainTourResponse>
+
+    // 4. 메인에서 Search
+    @GET("api/main/search/keyword")
+    fun getMainSearchData(
+            @Header("token") token : String
+    )
 }

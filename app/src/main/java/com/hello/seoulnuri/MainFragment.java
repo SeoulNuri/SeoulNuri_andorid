874b98851ce7071f3312283f349053a0381dc188
Woo <jhsw0375@gmail.com>
2018-09-16 23:31:56 +0900

    통신 구현 및 View 일부 수정

--- app/src/main/java/com/hello/seoulnuri/MainFragment.java
+++ app/src/main/java/com/hello/seoulnuri/MainFragment.java
@@ -8,7 +8,6 @@ import android.graphics.drawable.LayerDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.hello.seoulnuri.view.search.SearchActivity;
 
 
 /**
---
+++ app/src/main/java/com/hello/seoulnuri/view/search/SearchAdapter.kt
@@ -0,0 +1,26 @@
+package com.hello.seoulnuri.view.search
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by VictoryWoo
+ */
+class SearchAdapter : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): SearchViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: SearchViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class SearchViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}

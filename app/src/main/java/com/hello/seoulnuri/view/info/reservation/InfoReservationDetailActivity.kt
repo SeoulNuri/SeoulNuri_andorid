9fcc96276b92c4e36afa1a8d83d2d32a6833a06b
Woo <jhsw0375@gmail.com>
2018-09-23 20:28:48 +0900

    info 뷰 완성함

--- app/src/main/java/com/hello/seoulnuri/view/info/reservation/InfoReservationDetailActivity.kt
+++ app/src/main/java/com/hello/seoulnuri/view/info/reservation/InfoReservationDetailActivity.kt
@@ -1,4 +1,4 @@
-package com.hello.seoulnuri.view.info
+package com.hello.seoulnuri.view.info.reservation
 
 import android.content.Intent
 import android.graphics.Color
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialog
 import android.view.View
-import android.widget.LinearLayout
 import com.hello.seoulnuri.R
 import com.hello.seoulnuri.base.Init
 import com.hello.seoulnuri.info.CommentActivity

96de20fa6ae00880663bce4475f66d6c1681cc9a
Woo <jhsw0375@gmail.com>
2018-09-26 16:34:49 +0900

    분기 완성

--- app/src/main/java/com/hello/seoulnuri/model/search/filter/FilterData.kt
+++ app/src/main/java/com/hello/seoulnuri/model/search/filter/FilterData.kt
@@ -4,6 +4,8 @@ package com.hello.seoulnuri.model.search.filter
  * Created by VictoryWoo
  */
 data class FilterData (
+        var filter_type : String,
         var filter_detail : String,
-        var filter_status : Boolean
+        var filter_status : Boolean,
+        var filter_value : Int
 )
--- app/src/main/java/com/hello/seoulnuri/utils/custom/BookmarkDialog.kt
+++ app/src/main/java/com/hello/seoulnuri/utils/custom/BookmarkDialog.kt
@@ -6,24 +6,35 @@ import android.os.Bundle
 import android.view.View
 import com.hello.seoulnuri.R
 import com.hello.seoulnuri.base.Init
+import com.hello.seoulnuri.network.ApplicationController
+import com.hello.seoulnuri.network.NetworkService
+import com.hello.seoulnuri.utils.SharedPreference
 import kotlinx.android.synthetic.main.course_bookmark_dialog.*
 
 /**
  * Created by VictoryWoo
  */
-class BookmarkDialog(context : Context) : Dialog(context), View.OnClickListener,Init{
+class BookmarkDialog(context: Context) : Dialog(context), View.OnClickListener, Init {
     override fun init() {
         btn_bookmark_ok.setOnClickListener(this)
+        SharedPreference.instance!!.load(context!!)
+        networkService = ApplicationController.instance!!.networkService
+
     }
 
     override fun onClick(v: View?) {
-        when(v!!){
-            btn_bookmark_ok->{
+        when (v!!) {
+            btn_bookmark_ok -> {
+                requestTourBookmark()
                 dismiss() // 혹은 여기서 통신 진행하거나
             }
         }
     }
 
+
+    fun requestTourBookmark(){
+    }
+    lateinit var networkService: NetworkService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(LAYOUT)

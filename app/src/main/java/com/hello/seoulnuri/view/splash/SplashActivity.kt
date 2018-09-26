7b78da2af7e721dbe29f118b3bef96c58e894ca6
Woo <jhsw0375@gmail.com>
2018-09-26 13:14:17 +0900

    로그인 분기

--- app/src/main/java/com/hello/seoulnuri/view/splash/SplashActivity.kt
+++ app/src/main/java/com/hello/seoulnuri/view/splash/SplashActivity.kt
@@ -5,14 +5,20 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.hello.seoulnuri.R
+import com.hello.seoulnuri.base.Init
+import com.hello.seoulnuri.utils.SharedPreference
 import com.hello.seoulnuri.view.login.LoginActivity
 import com.hello.seoulnuri.view.main.MainActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), Init {
+    override fun init() {
+        SharedPreference.instance!!.load(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        init()
 /*
         try {
             val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
@@ -32,8 +38,13 @@ class SplashActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
 
         handler.postDelayed({
-            startActivity(intent)
-            finish()
+            if(SharedPreference.instance!!.getPrefStringData("data")!!.isEmpty()){
+                startActivity(intent)
+                finish()
+            }else{
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+
         }, 3000)
     }
 

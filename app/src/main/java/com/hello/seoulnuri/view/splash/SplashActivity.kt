5888b08373f6962ee800b134e2016fe38bfe0197
Woo <jhsw0375@gmail.com>
2018-09-27 17:10:47 +0900

    수정 작업

--- app/src/main/java/com/hello/seoulnuri/view/splash/SplashActivity.kt
+++ app/src/main/java/com/hello/seoulnuri/view/splash/SplashActivity.kt
@@ -1,15 +1,19 @@
 package com.hello.seoulnuri.view.splash
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
 import android.util.Log
 import com.hello.seoulnuri.R
 import com.hello.seoulnuri.base.Init
 import com.hello.seoulnuri.utils.SharedPreference
 import com.hello.seoulnuri.view.login.LoginActivity
 import com.hello.seoulnuri.view.main.MainActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class SplashActivity : AppCompatActivity(), Init {
     override fun init() {
@@ -21,7 +25,7 @@ class SplashActivity : AppCompatActivity(), Init {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         init()
-/*
+
         try {
             val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
             for (signature in info.signatures) {
@@ -34,7 +38,7 @@ class SplashActivity : AppCompatActivity(), Init {
         } catch (e: NoSuchAlgorithmException) {
             e.printStackTrace()
         }
-*/
+
 
         val handler = Handler()
         val intent = Intent(this, LoginActivity::class.java)

c8421b8fe728e897d3c10b982330497d8fdd0b76
장용범 <dnwkqk@gmail.com>
2018-09-30 20:37:34 +0900

    add planner path more data

--- app/src/main/java/com/hello/seoulnuri/model/planner/PlannerArroundData.kt
+++ app/src/main/java/com/hello/seoulnuri/model/planner/PlannerArroundData.kt
@@ -4,5 +4,8 @@ data class PlannerArroundData (
         var tour_idx: Int,
         var tour_name: String,
         var tour_latitude: Double,
-        var tour_longitude: Double
+        var tour_longitude: Double,
+        var tour_addr: String,
+        var tour_star: Double,
+        var tour_star_count: Int
 )
